558b83dc90e38661e39671a563c235d71c79cd48
DanielSR1 <danielsrcontato@gmail.com>
2022-03-13 01:44:05 -0300

    instagram python

--- Estudos fora do curso/Como-baixar-publicacoes-do-instagram-com-python.py
+++ Estudos fora do curso/Como-baixar-publicacoes-do-instagram-com-python.py
@@ -1,10 +1,14 @@
 from datetime import date, datetime
 import instaloader
 L=instaloader.Instaloader()
+#fazer login na sua conta do instagram#
 L.login('insira aqui seu login do instagram','insira sua senha')
-posts=instaloader.Profile.from_username(L.context, "#insira aqui o nome do usuario para download").get_posts()
+#carregar todos os posts do perfil escolhido#
+posts=instaloader.Profile.from_username(L.context, "insira aqui o nome do usuario para download").get_posts()
+#periodo que deseja baixar os posts#
 SINCE=datetime(2022, 3, 2)
 UNTIL=datetime(2022, 3, 2)
+#percorre os posts e baixa apenas os que estão na data desejada#
 for post in posts:
     if (post.date >= SINCE) and (post.date <= UNTIL):
         print(post.date)
